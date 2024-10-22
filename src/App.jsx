2fae78ddece1f099a1ade4b9da2821ad7e10afa5
Abhinav Pandey <abhinavpandey1230@gmail.com>
2024-10-22 21:27:30 +0530

    Update all form types with Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import SurveyComponent from "./components/Survey";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import SurveyComponent from "./components/SurveyComponent";
+import "./App.css"
 
 function App() {
   return (
-    <>
-      <SurveyComponent />
-    </>
+    <Router>
+      <div>
+        <h1>Survey Dashboard</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/survey/self">Self Feedback</Link>
+            </li>
+            <li>
+              <Link to="/survey/individual">Individual Feedback</Link>
+            </li>
+            <li>
+              <Link to="/survey/team">Team Feedback</Link>
+            </li>
+            <li>
+              <Link to="/survey/peer">Peer Feedback</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/survey/self" element={<SurveyComponent surveyUrl="http://localhost:3000/api/survey/self" />} />
+          <Route path="/survey/individual" element={<SurveyComponent surveyUrl="http://localhost:3000/api/survey/individual" />} />
+          <Route path="/survey/team" element={<SurveyComponent surveyUrl="http://localhost:3000/api/survey/team" />} />
+          <Route path="/survey/peer" element={<SurveyComponent surveyUrl="http://localhost:3000/api/survey/peer" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/SurveyComponent.jsx
+++ src/components/SurveyComponent.jsx
@@ -3,13 +3,14 @@ import { SurveyModel } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 import 'survey-core/defaultV2.min.css';
 import { surveyParser } from '../assets/surveyParser';
+import { DefaultFonts } from "survey-creator-core";
 
-export default function SurveyComponent() {
+export default function SurveyComponent({ surveyUrl }) {
   const [surveyData, setSurveyData] = useState(null);
 
   const fetchSurveyData = async () => {
     try {
-      const res = await fetch('http://localhost:3000/api/survey/');
+      const res = await fetch(surveyUrl);
       const data = await res.json();
       const updatedData = await surveyParser(data);
       setSurveyData(updatedData);
@@ -17,18 +18,25 @@ export default function SurveyComponent() {
       console.error('Error fetching survey:', error);
     }
   };
+
   useEffect(() => {
     fetchSurveyData();
-  }, []);
+  }, [surveyUrl]);
+
   const handleResults = useCallback((sender) => {
     const results = JSON.stringify(sender.data);
     alert(results);
   }, []);
+
   if (!surveyData) {
     return <div>Loading...</div>;
   }
+
   const survey = new SurveyModel(surveyData);
   survey.onComplete.add(handleResults);
+  DefaultFonts.push(
+      "Montserrat, sans-serif"
+  );
 
   return <Survey model={survey} />;
 }
